6658a501839235403a5838521cdea1bf3a888585
bryantlta <bryantl@berkeley.edu>
2019-06-06 16:01:23 -0700

    completed skyhook tool

--- tools/getAccessPoints.py
+++ tools/getAccessPoints.py
@@ -1,11 +1,11 @@
 # Using https://github.com/kootenpv/access_points without having to pip install access_points.
 
 def scanAccessPoints():
-    # Get all access points. 
+    # Get all access points for any wifi adapter. 
 
     import access_pointsPackage as access
 
-    wifi_scanner = access.get_scanner("wlp2s0")
+    wifi_scanner = access.get_scanner()
     points = wifi_scanner.get_access_points()
 
     for p in points:
--- tools/skyHookApiIpQuery.py
+++ tools/skyHookApiIpQuery.py
@@ -1,8 +1,8 @@
 def infoFromIp(version, ip, key, user, prettyPrint = False):
     # Get info from IP address using the skyHook ip location api. 
-
     import urllib
     import json
+    import argparse
 
     versionString = "version=" + str(version)
     ipString = "&ip=" + str(ip)
@@ -17,3 +17,26 @@ def infoFromIp(version, ip, key, user, prettyPrint = False):
     jsonResponse = json.loads(resp.read())
     print(jsonResponse)
     return jsonResponse
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Get info about IP.") 
+    parser.add_argument('--ip', dest='ip', required=True) 
+    parser.add_argument('--version', dest='version', required=True)
+    parser.add_argument('--prettyPrint', dest='print', required=False) 
+    parser.add_argument('--key', dest='key', required=True)
+    parser.add_argument('--user', dest='user', required=True)
+
+
+    args = parser.parse_args()
+    ip = str(args.ip)
+    version = str(args.version)
+    prettyPrint = str(args.print)
+    key = str(args.key)
+    user = str(args.user)
+
+    return infoFromIp(version, ip, key, user, prettyPrint)
+
+if __name__ == '__main__':
+    main()
