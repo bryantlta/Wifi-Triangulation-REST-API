d27fff394f47158573b6512081066954c0c9dfbf
bryantlta <bryantl@berkeley.edu>
2019-06-07 15:18:22 -0700

    postRequests

--- postRequests.py
+++ postRequests.py
@@ -6,4 +6,13 @@ def postRequestXML(location_api_endPoint, xml_string):
     header = {'Content-Type': 'text/xml'}
 
     postRequest = requests.post(location_api_endPoint, data = xml_string, headers = header)
+    return postRequest
+
+def postRequestJson(location_api_endPoint, json_string):
+    import requests
+
+    header = {'Content-Type': 'application/json'}
+
+    postRequest = requests.post(location_api_endPoint, data = json_string, headers = header)
+    return postRequest
 
--- readArgs.py
+++ readArgs.py
@@ -1,8 +1,10 @@
-# Grab arguments from command line, find that file, and read that file into string.
+# Grab arguments from command line 
+#   (assuming that file being read is the second arg (first being this py file)), 
+#   find that file, and read that file into string.
 
 def readArgs():
 	import sys
-    
+
 	in_filename = sys.argv[1]
 	in_file = open(in_filename, 'r')
 	string = in_file.read()
