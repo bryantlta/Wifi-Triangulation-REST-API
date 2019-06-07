9417d289c67ef6b88c2fb1f358067269f1e9fee3
bryantlta <bryantl@berkeley.edu>
2019-06-07 15:47:13 -0700

    tried colons for skyhook

--- skyHookPostRequest.py
+++ skyHookPostRequest.py
@@ -1,11 +1,21 @@
 # Precision location POST request from skyhook REST api. 
+
+# Skyhook request requies that bssids do NOT have colons. 
+# Signal strength and quality mean the same thing and negative signs do not matter. 
         
 def skyHookRequest():
     from tools import readArgs
     from tools import postRequests
     
     api_location_endPoint = 'https://global.skyhookwireless.com/wps2/location'
-    xml_string = readArgs.readArgs()
+    xml_string = readArgs.readArgs(1)
 
     postRequest = postRequests.postRequestXML(api_location_endPoint, xml_string)
-    return postRequest 
+    print(postRequest.text)
+    return postRequest
+
+def main():
+    return skyHookRequest()
+
+if __name__ == '__main__':
+    main()   
--- tools/readArgs.py
+++ tools/readArgs.py
@@ -2,16 +2,16 @@
 #   (assuming that file being read is the second arg (first being this py file)), 
 #   find that file, and read that file into string.
 
-def readArgs():
+def readArgs(k):
 	import sys
 
-	in_filename = sys.argv[1]
+	in_filename = sys.argv[k]
 	in_file = open(in_filename, 'r')
 	string = in_file.read()
 	return string 
 
 def main():
-    return readArgs()
+    return readArgs(1)
 
 if __name__ == 'main':
     main()
