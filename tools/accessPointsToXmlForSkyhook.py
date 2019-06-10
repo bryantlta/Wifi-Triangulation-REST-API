ca9bb742cf795708b87129e7445d8f6e6ae3b70b
bryantlta <bryantl@berkeley.edu>
2019-06-09 22:16:16 -0700

    completed the fixed version of wifi triangulation

--- tools/accessPointsToXmlForSkyhook.py
+++ tools/accessPointsToXmlForSkyhook.py
@@ -3,33 +3,37 @@
 # https://stackabuse.com/reading-and-writing-xml-files-in-python/
 # https://bugs.python.org/issue10942 (issue with why ElementTree.tostring returns byte instead of string)
 
-def accessPointsToXmlForSkyHook(accessPoints, apiKey, deviceId, xmlFile):
+def accessPointsToXmlForSkyHook(accessPoints, apiKey, deviceId, xmlFile = 'xmlFile.xml'):
     import xml.etree.ElementTree as ElementTree
 
     # Location RQ values:
     LocationRQ = ElementTree.Element('LocationRQ')
 
     LocationRQ.set('xmlns', "http://skyhookwireless.com/wps/2005")
-    LocationRQ.set('version', "2.25")
+    LocationRQ.set('version', "2.26")
     LocationRQ.set('street-address-lookup', "full")
-    LocationRQ.set('timezone-lookup', "true")
-
+    
     authentication = ElementTree.SubElement(LocationRQ, 'authentication')
-    authentication.set('key', apiKey)
-    authentication.set('username', deviceId)
+    authentication.set('version', '2.2')
+
+    key = ElementTree.SubElement(authentication, 'key')
+    key.set('key', apiKey)
+    key.set('username', deviceId)
+    
 
     for point in accessPoints:
         accessPoint = ElementTree.SubElement(LocationRQ, 'access-point')
-        
-        bssid = point.bssid.replace(':','')
-        quality = point.quality
 
-        accessPoint.set('mac', bssid)
-        accessPoint.set('signal-strength', quality)
+        bssid = ElementTree.SubElement(accessPoint, 'mac')
+        bssid.text = str(point.bssid).replace(':', '')
+
+        quality = ElementTree.SubElement(accessPoint, 'signal-strength')
+        quality.text = str(point.quality)
     
     mydata = ElementTree.tostring(LocationRQ, encoding="unicode") 
     myfile = open(xmlFile, "w")
     myfile.write(mydata)
+    return xmlFile 
 
 
 
--- wifiTriangulation.py
+++ wifiTriangulation.py
@@ -3,11 +3,24 @@ from tools import accessPointsMethods
 from tools import postRequests
 from tools import readArgs
 from tools import accessPointsToXmlForSkyhook
+import sys 
 
 def main():
+    # [skyHook API Key, deviceId, optional xmlFile]
+    args = sys.argv
+
     points = accessPointsMethods.scanAccessPoints()
-    
-    accessPointsToXmlForSkyhook.accessPointsToXmlForSkyHook()
+
+    xmlFile = accessPointsToXmlForSkyhook.accessPointsToXmlForSkyHook(points, args[1], args[2])
+
+    in_file = open(xmlFile, 'r')
+	xmlString = xmlFile.read()
+
+	api_location_endPoint = 'https://global.skyhookwireless.com/wps2/location'
+    postRequests.postRequestXML(api_location_endPoint, xmlString)
+    print(postRequest.text)
+    return postRequest
+
 
 
 if __name__ == "__main__":
