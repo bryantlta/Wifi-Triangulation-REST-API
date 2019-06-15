aaa7bc9d654a6602bdcdc68c4e1862c2b2712d9d
bryantlta <bryantl@berkeley.edu>
2019-06-15 12:52:06 -0700

    added wifi connections

--- nodeJSApi/wifiTriangulation.js
+++ nodeJSApi/wifiTriangulation.js
@@ -6,20 +6,49 @@ app.set('view engine', 'ejs')
 var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded{enabled: false}
 
-var wifi = require('node-wifi')
-wifi.init({
-  iface: null
-})
+var getAccessPoints = function(){
+  //Import NPM node-wifi.
+  var wifi = require('node-wifi')
 
-wifi.scan(function(err, networks) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(networks)
-      // [{ssid, bssid, signal_level, quality)}]
-  }
+  // Initialize the wifi object.
+  wifi.init({
+    iface: null
+  })
+
+  // Access Points PROMISE.
+  wifi.scan().then(function (networks) {
+    return networks
+  }).catch(function (error) {
+    console.log(error)
+  })
 }
 
+var fs = require('fs')
+var xml2js = require('xml2js')
+
+var getJson = function(html){
+  var parser = new xml2js.Parse();
+
+  fs.readFile(__dirname + html, function(err, data) {
+    parser.parseString(data, function (err, result) {
+      console.dir(result);
+      console.log('Done');
+      return JSON.stringify(result)
+    })
+  })
+}
+
+var json = getJson('index.html')
+
+root = element('entry')
+root.set('xmlns', 'http://www.w3.org/2005/Atom...')
+
+tenantId = subElement(root, 'TenantId');
+tenantId.text = '12345';
+
+etree = new ElementTree(root)
+xml = etree.write({'xml_declaration': false});
+console.log(xml);
 
 // PROJECT IDEA. We can try connecting to networks programmatically
 // Brute force guess passwords. Outputs the password that works!
