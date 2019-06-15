d70ba2cc58c6ca603713349403e634a90d0a4d28
bryantlta <bryantl@berkeley.edu>
2019-06-15 12:28:32 -0700

    progress

--- nodeJSApi/wifiTriangulation.js
+++ nodeJSApi/wifiTriangulation.js
@@ -1,5 +1,42 @@
 var express = require('express.js')
-
 var app = express()
 
+app.set('view engine', 'ejs')
 
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded{enabled: false}
+
+var wifi = require('node-wifi')
+wifi.init({
+  iface: null
+})
+
+wifi.scan(function(err, networks) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(networks)
+      // [{ssid, bssid, signal_level, quality)}]
+  }
+}
+
+
+// PROJECT IDEA. We can try connecting to networks programmatically
+// Brute force guess passwords. Outputs the password that works!
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + 'index.html')
+}
+
+app.post('/key',  urlencodedParser, function(req, res){
+  // Get access points.
+  //  all of the access point names and qualities.
+  // Grab computer name and encode it.
+  // Turn access points to XML string.
+  // POST request to Skyhook
+  // XML response to JSON
+  // POST RESPONSE the JSON and display important information.
+
+  req.body.key
+  res.render(...esj, {body: req.body}
+})
